1ce89c3ecdef1997e9658ad6f8fb920081829930
BangtanBTS <silvaniaalvesrbd@gmail.com.br>
2025-04-04 08:55:39 -0300

    Uso de Classe com Atributos, Construtor e Métodos

--- aluno.js
+++ aluno.js
@@ -1,13 +1,30 @@
+class Aluno {
+   constructor(nome, idade, curso, notaFinal) {
+       this.nome = nome;
+       this.idade = idade;
+       this.curso = curso;
+       this.notaFinal = parseFloat(notaFinal);
+   }
+
+   isAprovado() {
+       return this.notaFinal >= 7;
+   }
+
+   toString() {
+       return `Nome: ${this.nome}, Idade: ${this.idade}, Curso: ${this.curso}, Nota Final: ${this.notaFinal}`;
+   }
+}
+
 document.getElementById('formulario-aluno').addEventListener('submit', function(event) {
-   event.preventDefault(); 
+   event.preventDefault();
 
    const nome = document.getElementById('nome').value;
    const idade = document.getElementById('idade').value;
    const curso = document.getElementById('curso').value;
-   const nota = parseFloat(document.getElementById('nota').value);
+   const nota = document.getElementById('nota').value;
 
-   const aluno = { nome, idade, curso, nota };
-
+   // Criando uma instância da classe Aluno
+   const aluno = new Aluno(nome, idade, curso, nota);
    alunos.push(aluno);
 
    atualizarTabela();
@@ -17,7 +34,7 @@ document.getElementById('formulario-aluno').addEventListener('submit', function(
 
 let alunos = [];
 
-// exibir a tabela de alunos
+// Exibir a tabela de alunos
 function atualizarTabela() {
    const tabela = document.getElementById('tabela-aluno').getElementsByTagName('tbody')[0];
    tabela.innerHTML = ''; // limpa a tabela antes de adicionar os novos dados
@@ -28,7 +45,7 @@ function atualizarTabela() {
            <td>${aluno.nome}</td>
            <td>${aluno.idade}</td>
            <td>${aluno.curso}</td>
-           <td>${aluno.nota}</td>
+           <td>${aluno.notaFinal}</td>
            <td>
                <button class="editar-btn" data-index="${index}">Editar</button>
                <button class="excluir-btn" data-index="${index}">Excluir</button>
@@ -59,7 +76,7 @@ function adicionarEventosBotao() {
    });
 }
 
-// função editar um aluno
+// função para editar um aluno
 function editarAluno(index) {
    const aluno = alunos[index];
 
@@ -67,14 +84,14 @@ function editarAluno(index) {
    document.getElementById('nome').value = aluno.nome;
    document.getElementById('idade').value = aluno.idade;
    document.getElementById('curso').value = aluno.curso;
-   document.getElementById('nota').value = aluno.nota;
+   document.getElementById('nota').value = aluno.notaFinal;
 
    // remove o aluno da lista para poder editar
    alunos.splice(index, 1);
    atualizarTabela();
 }
 
-// função excluir um aluno
+// função para excluir um aluno
 function excluirAluno(index) {
    alunos.splice(index, 1);
    atualizarTabela();
